83b41f7b65e71535a877fa0b3cd8c74e2212351e
BayerTobias <tbayer2@gmx.de>
2024-01-30 18:49:43 +0100

    Create funktion in views

--- todo/views.py
+++ todo/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import permissions, viewsets
 from .models import Todo
 from .serializers import TodoSerializer
+from django.core import serializers
+from django.http import HttpResponse
 
 
 class TodoViewSet(viewsets.ModelViewSet):
@@ -12,3 +14,12 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by("-created_at")
     serializer_class = TodoSerializer
     permission_classes = []  # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(
+            title=request.POST.get("title", ""),
+            description=request.POST.get("description", ""),
+            user=request.user,
+        )
+        serialized_obj = serializers.serialize("json", [todo])
+        return HttpResponse(serialized_obj, content_type="application/json")
